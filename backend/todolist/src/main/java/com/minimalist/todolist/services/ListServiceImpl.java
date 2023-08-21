6ece039d4655653dfbac92aa7a29fb062b0e376a
Piyush Terkar <terkar.piyush3@gmail.com>
2023-08-21 11:53:16 +0530

    BACKEND: ADD: CRUD Service implementation

--- backend/todolist/src/main/java/com/minimalist/todolist/services/ListServiceImpl.java
+++ backend/todolist/src/main/java/com/minimalist/todolist/services/ListServiceImpl.java
@@ -1,27 +1,47 @@
 package com.minimalist.todolist.services;
 
+import com.minimalist.todolist.mappers.ListMapper;
 import com.minimalist.todolist.model.ListDTO;
+import com.minimalist.todolist.repositories.ListRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Service
+@RequiredArgsConstructor
 public class ListServiceImpl implements ListService {
+    
+    private ListMapper listMapper;
+    private ListRepository listRepository;
+    
     @Override
     public Flux<ListDTO> getAllLists() {
-        return null;
+        return listRepository.findAll()
+                .map(listMapper::listToListDTO);
     }
     
     @Override
-    public Mono<ListDTO> createList(ListDTO listDTO) {
-        return null;
+    public Mono<ListDTO> createList(Mono<ListDTO> listDTO) {
+        return listDTO.map(listMapper::listDtoToList)
+                .flatMap(listRepository::save)
+                .map(listMapper::listToListDTO);
     }
     
     @Override
     public Mono<ListDTO> updateList(String listId, ListDTO listDTO) {
-        return null;
+        return listRepository.findById(listId)
+                .map(foundList -> {
+                    foundList.setEmoji(listDTO.getEmoji());
+                    foundList.setTitle(listDTO.getTitle());
+                    return foundList;
+                })
+                .flatMap(listRepository::save)
+                .map(listMapper::listToListDTO);
     }
     
     @Override
     public Mono<Void> deleteList(String listId) {
-        return null;
+        return listRepository.deleteById(listId);
     }
 }
