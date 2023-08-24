4e26f4fd4f2a6d2450fed1e1ef1cc5cc7c4ca7d3
Piyush Terkar <terkar.piyush3@gmail.com>
2023-08-24 11:46:34 +0530

    BACKEND(AUTH): ADD: Database connection

--- backend/AuthServer/src/main/java/com/Minimalist/AuthServer/config/MongoConfig.java
+++ backend/AuthServer/src/main/java/com/Minimalist/AuthServer/config/MongoConfig.java
@@ -1,4 +1,19 @@
 package com.Minimalist.AuthServer.config;
 
-public class MongoConfig {
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+
+public class MongoConfig extends AbstractReactiveMongoConfiguration {
+    
+    @Bean
+    MongoClient mongoClient(){
+        return MongoClients.create();
+    }
+    
+    @Override
+    protected String getDatabaseName() {
+        return "minimalist";
+    }
 }
