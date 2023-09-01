a08ba71b704170c811057a74011502cfe484bb0f
Piyush Terkar <terkar.piyush3@gmail.com>
2023-09-01 15:55:16 +0530

    BACKEND: REMOVE: AUTHSERVER microservice

--- backend/AuthServer/src/main/java/com/Minimalist/AuthServer/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Minimalist.AuthServer.repositories;
-
-import com.Minimalist.AuthServer.entities.User;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import reactor.core.publisher.Mono;
-
-public interface UserRepository extends ReactiveMongoRepository<User, String> {
-    Mono<User> findByUserName(String userName);
-}
